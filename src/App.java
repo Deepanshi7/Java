93ce1db953ca7b6d02bcb620c0ba28c7977e07cf
Deepanshi7 <deepanshi654321@gmail.com>
2024-12-13 14:32:03 +0530

    new

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.io.FileWriter;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        FileWriter fout = new FileWriter("file.txt");
+        fout.write("this file is created by anshu");
+        fout.close();
+        System.out.println( "write operation succesful");
     }
 }
---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String args[]){
+        String line =new String("hello");
+        String second =new String("hello");
+        String third = "good morning";
+        System.out.println(third);
+        System.out.println(second);
+        System.out.println(line);
+    }
+}
